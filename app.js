103e61ee27308d0152eec7db0800e869e7510518
yuluo-ding <luoshatter@gmail.com>
2015-04-08 22:12:23 +0800

    updata

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 /**
  * Created by yu on 2015/4/8.
  */
-
 $(document).ready(function () {
    $(window).on("load",function(){
        imgLocation();
-       var dataImg = {"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpg"},{"src":"8.jpg"}]}
+       var dataImg = {"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"}]};
        window.onscroll = function () {
             if(scrollside()){
                 $.each(dataImg.data, function (index,value) {
-                    var box = $("<div>").addClass("box").appendTo($("container"));
+                    var box = $("<div>").addClass("box").appendTo($("#container"));
                     var content =  $("<div>").addClass("content").appendTo(box);
+                    //                    console.log("./img/"+$(value).attr("src"));
                     $("<img>").attr("src","./img/"+$(value).attr("src")).appendTo(content);
                 });
                 imgLocation();
@@ -18,6 +18,7 @@ $(document).ready(function () {
        };
    });
 });
+
 
 function scrollside(){
     var box = $(".box");
@@ -31,17 +32,22 @@ function imgLocation(){
     var box = $(".box");
     var boxWidth = box.eq(0).width();
     var num = Math.floor($(window).width()/boxWidth);
-    var boxArr = [];
+    var boxArr=[];
     box.each(function(index,value){
-       var boxHeight = box.eq(index).height();
+//        console.log(index+"--"+value);
+        var boxHeight = box.eq(index).height();
         if(index<num){
-            boxArr[index] = boxHeight;
+            boxArr[index]= boxHeight;
+//            console.log(boxHeight);
         }else{
-            var minBoxHeight = Math.min.apply(null,boxArr);
-            var minboxIndex = $.inArray(minBoxHeight,boxArr);
+            var minboxHeight = Math.min.apply(null,boxArr);
+//            console.log(minboxHeight);
+            var minboxIndex = $.inArray(minboxHeight,boxArr);
+//            console.log(minboxIndex);
+//            console.log(value);
             $(value).css({
                 "position":"absolute",
-                "top":minBoxHeight,
+                "top":minboxHeight,
                 "left":box.eq(minboxIndex).position().left
             });
             boxArr[minboxIndex]+=box.eq(index).height();
